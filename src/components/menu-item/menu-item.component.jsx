92653de087852c5cda77076383de260e6666b3c5
saroj <sarojsitaula@gmail.com>
2021-05-19 22:31:08 +0300

    routing pages

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import {withRouter} from 'react-router-dom'
 import './menu-item.styles.scss'
-const MenuItem=({title,imageUrl, size})=>(
-<div style={{
-    backgroundImage:`url(${imageUrl})`
-}} className={`${size} menu-item`}>
+const MenuItem=({title,imageUrl, size, history, match, linkUrl})=>(
+
+<div  className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
+                <div className='background-image' style={{
+                    backgroundImage:`url(${imageUrl})`
+                    }}/>
                 <div className='content'>
-                    <h1 className='title'>{title}</h1>
+                    <h1 className='title'>{title.toUpperCase()}</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
-            </div>
+</div>
 )
-export default MenuItem;
+export default withRouter(MenuItem);
 
     

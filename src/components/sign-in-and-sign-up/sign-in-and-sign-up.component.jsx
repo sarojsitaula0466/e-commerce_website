1db6be62e6e18d1a6caddf61e3c297e7379e632c
saroj <sarojsitaula@gmail.com>
2021-05-25 22:34:05 +0300

    signin form created

--- src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import SignIn from '../sign-in/sign-in.component'
 import './sign-in-and-sign-up.styles.scss'
 
 const SignInAndSignUp=()=>(
-<div className='sign-in-and-sign-up'>Sign In</div>
+<div className='sign-in-and-sign-up'>
+    <SignIn/>
+</div>
 )
 export default SignInAndSignUp;

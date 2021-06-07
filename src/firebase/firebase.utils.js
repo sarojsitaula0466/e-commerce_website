1699b4f985d4652450f9ff9bcd4fd23371e825f2
saroj <sarojsitaula@gmail.com>
2021-06-07 21:58:46 +0300

    firebase setup done

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,24 +1,33 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
-const config={
-        apiKey: "AIzaSyBQMd2Lxx2Q14uTvwaySbvypIMioS1rWg4",
-        authDomain: "e-commerce-493b5.firebaseapp.com",
-        projectId: "e-commerce-493b5",
-        storageBucket: "e-commerce-493b5.appspot.com",
-        messagingSenderId: "517663191570",
-        appId: "1:517663191570:web:306d81c322e4ed280c1ecb",
-        measurementId: "G-6K4Z2GHLLR"
+const config = {
+  apiKey: "AIzaSyBQMd2Lxx2Q14uTvwaySbvypIMioS1rWg4",
+  authDomain: "e-commerce-493b5.firebaseapp.com",
+  projectId: "e-commerce-493b5",
+  storageBucket: "e-commerce-493b5.appspot.com",
+  messagingSenderId: "517663191570",
+  appId: "1:517663191570:web:306d81c322e4ed280c1ecb",
+  measurementId: "G-6K4Z2GHLLR",
+};
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+  }
 };
 firebase.initializeApp(config);
 
-export const auth= firebase.auth();
-export const firestore= firebase.firestore();
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
-const provider= new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt:'select_account'});
-export const signInWithGoogle=()=>auth.signInWithPopup(provider);
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;

49c029d5b984adb3dbf08e09b66b32a3e151085d
saroj <sarojsitaula@gmail.com>
2021-08-04 15:34:44 +0300

    backend feature for stripe payment added

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios'
+
 
 const StripeCheckoutButton=({price})=>{
 const priceForStripe=price*100;
 const publishableKey='pk_test_51JIUOxHY2BtAt4F72NTaC68EY3fTox72FCFjF7tTo7zzdDy7lhEcOdymz9X0484PJUpcER5zlAtCKhx5LOWEa11a00BB2CcMB0'
 const onToken=token=>{
-    console.log(token);
-    alert('Payment Successful')
+   axios({
+       url:'payment',
+       method:'post',
+       data:{
+           amount:priceForStripe,
+           token
+       }
+   }).then(response=>{
+       alert('Payment successful')
+   }).catch(error=>{
+       console.log('Payment error:', JSON.parse(error));
+       alert('There was an issue with your payment. Please make sure you use the provided credit card')
+   })
 }
 return (
     <StripeCheckout

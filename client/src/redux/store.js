11e1886775e60186fd4c2a6721278beac8d951d4
saroj <sarojsitaula@gmail.com>
2021-08-25 12:22:36 +0300

    logger deleted from production

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,7 +4,10 @@ import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger, thunk];
+const middlewares = [thunk];
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
 export default { store, persistor };
